36897203bbf20375911209572cf55b672dcbd5df
Nicowski25 <nicola.faedo0610@gmail.com>
2023-03-29 17:51:39 +0200

    carousel

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,34 @@ const { createApp } = Vue
 createApp({
   data() {
     return {
-      message: 'Hello Vue!'
+      activeImage: 0,
+      carousel: {
+          images: [
+            "./assets/img/01.webp",
+            "./assets/img/01.webp",
+            "./assets/img/02.webp",
+            "./assets/img/03.webp",
+            "./assets/img/04.webp",
+            "./assets/img/05.webp"
+            ],
+        }
     }
-  }
+    },
+    methods: {
+        next() {
+            this.activeImage++
+            if (this.activeImage == this.carousel.images.length) {
+                this.activeImage = 0
+            }    
+        },
+        prev() {
+            this.activeImage--
+            if (this.activeImage <= 0) {
+                this.activeImage = this.carousel.images.length
+            }   
+        },
+        changeImage(index) {
+            this.activeImage = index
+        }
+    }
 }).mount('#app')
